303f3d5ab15325b84e72090ee514073dd4946f84
Pavithrag0103 <pavithrag0103@gmail.com>
2023-10-20 17:51:37 +0530

    saved

--- sample.py
+++ sample.py
@@ -75,13 +75,107 @@ for i in range(1, rows + 1):
     # Print a new line at the end of each row
     print()
 
+# w3 resources - online python practise
+
+""" Write a Python program to construct the following pattern, using a nested for loop.
+
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+* * * * 
+* * * 
+* * 
+*     """
+
+rows=6
+for i in range(rows+1):     # 1 2 3 4 5 6
+    for j in range(1,i):    # (1,1)  (1,2)  (1,3)  (1,4)  (1,5)  (1,6)
+        print("*",end=" ")
+    print()
+for i in range(rows-1,0,-1):   #5 4 3 2
+    for j in range(1,i):    # (1,5) (1,4) (1,3) (1,2)
+        print("*",end=" ")
+    print()
 
 
 
+"""Write a Python program to construct the following pattern, using a nested loop number.
+Expected Output:
+
+1
+22
+333
+4444
+55555
+666666
+7777777
+88888888
+999999999"""
 
 
 
+row=9
+for i in range(1,row+1):  # 1 2 3 4 5 6 7 8 9
+    for j in range(0,i):    # (0,1) (0,2) (0,3).....
+        print(i,end=" ")
+    print()      # helps to move the next line after printing the num
 
+"""Write a Python program to create the multiplication table (from 1 to 10) of a number.
+Expected Output:
 
+Input a number: 6                                                       
+6 x 1 = 6                                                               
+6 x 2 = 12                                                              
+6 x 3 = 18                                                              
+6 x 4 = 24                                                              
+6 x 5 = 30                                                              
+6 x 6 = 36                                                              
+6 x 7 = 42                                                              
+6 x 8 = 48                                                              
+6 x 9 = 54                                                              
+6 x 10 = 60 """
 
+num=int(input("enter a num: "))
+for i in range(1,11):
+    print(f"{num} x {i} = {num*i}")
+
+"""Write a Python program to find the median of three values.
+Expected Output:
+
+Input first number: 15                                                  
+Input second number: 26                                                 
+Input third number: 29                                                  
+The median is 26.0 
+a = float(input("Input first number: "))
+b = float(input("Input second number: "))
+c = float(input("Input third number: "))
+if a > b:
+    if a < c:
+        median = a
+    elif b > c:
+        median = b
+    else:
+        median = c
+else:
+    if a > c:
+        median = a
+    elif b < c:
+        median = b
+    else:
+        median = c
+
+print("The median is", median)"""
+
+import datetime
+time=datetime.datetime.now()
+print(time)
+
+"""Write a Python program to sum two integers. However, if the sum is between 15 and 20 it will return 20"""
+num1=int(input("enter num1: "))
+num2=int(input("enter num2: "))
+num3=num1+num2
+if num3>=15 and num<=20:
+    print
 
